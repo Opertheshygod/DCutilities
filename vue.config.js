7cf4698d0e1831846bad9154a5623b6b1af7a54e
Копылов Михаил <kopylov.m@naasson.com>
2024-09-23 21:27:15 +0300

    +

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.exports = {
-    publicPath: '/DCutilities/' 
-  }
+export default {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/DCutilities/'  // Замените на имя вашего репозитория
+      : '/'
+  };
